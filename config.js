71bf666ba978b3c9ad518f18eee1c16ffb54d114
TwistPlay <twistplay@mail.ru>
2020-04-21 00:09:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
 
-	/**
-	 * Instructions on how to get this: https://redd.it/40zgse
-	 */
+
 	yourID: "473526373575819264",
 
 	setupCMD: "!kasdfasdfasdfasdfasdfvczxcvn)",
@@ -22,48 +20,20 @@ module.exports = {
 	Так же, после подтверждения, прошу вас прочесть правила в текстовом канале #❮🚫❯⥤rules. Чтобы у нас с вами потом не было проблем. Мы рассчитываем на адекватность, дружелюбность каждого участника на сервере.
 	`,
 	
-	/**
-	 * Must set this if "embed" is set to true
-	 */
+
 	embedFooter: "Подтверждения",
 	
 	roles: ["confirmed"],
 
-	/**
-	 * For custom emojis, provide the name of the emoji
-	 */
 	reactions: ["crosspeepo"],
 
-	/**
-	 * Set to "true" if you want all roles to be in a single embed
-	 */
 	embed: true,
 
-	/**
-	 * Set the embed color if the "embed" variable is et to "true"
-	 * Format:
-	 * 
-	 * #dd9323
-	 */
 	embedColor: "#8b00ff",
 
-	/**
-	 * Set to "true" if you want to set a thumbnail in the embed
-	 */
 	embedThumbnail: true,
 
-	/**
-	 * The link for the embed thumbnail if "embedThumbnail" is set to true
-	 */
 	embedThumbnailLink: "https://sun9-40.userapi.com/c206728/v206728499/8f166/hreTmG7O_CQ.jpg",
 
-	/**
-	 * You"ll have to set this up yourself! Read more below:
-	 * Please do not commit this token to the public if you contributed to this repository
-	 * or host your code anywhere online. Giving someone your bot's token is the equivalent
-	 * to giving someone the keys to your house and walking away!
-	 * 
-	 * https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
-	 */
 	botToken: "token"
 };
